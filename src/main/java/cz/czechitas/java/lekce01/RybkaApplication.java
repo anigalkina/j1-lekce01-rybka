d9b26247f7c1bc632ab702f8b15414bf72f815fc
annagalkina <anna.galkina@zonky.cz>
2023-02-07 20:12:09 +0100

    Rybka cyklus

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -30,6 +30,8 @@ public class RybkaApplication extends CzechitasGameApplication {
     protected void onStart() {
         kamen = new Kamen(200, 200);
         rybka = new Rybka(100, 100);
+        for (int i=0; i<20; i++)
+            rybka.plavatVpravo();
     }
 
     public static void main(String[] args) {
